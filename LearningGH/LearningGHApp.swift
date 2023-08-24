1fa8155fe3fb086179328018d2ddb7802bb92cba
Lester Sanchez <lester.sanchez1120@gmail.com>
2023-08-24 00:22:56 -0400

    Initial Commit

---
+++ LearningGH/LearningGHApp.swift
@@ -0,0 +1,17 @@
+//
+//  LearningGHApp.swift
+//  LearningGH
+//
+//  Created by Lester Sanchez on 8/24/23.
+//
+
+import SwiftUI
+
+@main
+struct LearningGHApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
